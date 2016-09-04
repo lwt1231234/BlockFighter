39c0dcad9ef4273d8891727485ee9a975521213c
Sniper Liu <lwt1231234@gmail.com>
2016-09-04 23:49:52 +0800

    2016.09.04
    1.有了基本的横向移动功能
    2.能够发射子弹，子弹超出范围自动销毁

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletControl : MonoBehaviour {
+	int MoveSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 NewPosition = transform.position+(Vector3.up *+ MoveSpeed*Time.deltaTime);
+		this.gameObject.GetComponent<Rigidbody2D> ().MovePosition(NewPosition);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public GameObject Bullet;
+	int MoveSpeed=10;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)||Input.GetKey (KeyCode.LeftArrow)) {
+			//transform.Translate (Vector3.right *- MoveSpeed*Time.deltaTime);
+			Vector3 NewPosition = transform.position+(Vector3.right *- MoveSpeed*Time.deltaTime);
+			this.gameObject.GetComponent<Rigidbody2D> ().MovePosition(NewPosition);
+		}
+		if (Input.GetKey (KeyCode.D)||Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 NewPosition = transform.position+(Vector3.right *+ MoveSpeed*Time.deltaTime);
+			this.gameObject.GetComponent<Rigidbody2D> ().MovePosition(NewPosition);
+		}
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Vector3 Position = transform.position + (Vector3.up * +0.4f);
+			print (Position);
+			Instantiate (Bullet, Position, Quaternion.identity);
+		}
+	}
+}
